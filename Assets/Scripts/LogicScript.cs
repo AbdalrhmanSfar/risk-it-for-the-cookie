f6d99d2a7b01e7ca669013fda2c5b2d494a31bdd
Sima <simasandouk@gmail.com>
2025-08-26 15:06:08 +0300

    fixed merge issues

---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class LogicScript : MonoBehaviour
+{
+    // edit the variables on this script/this object only
+    public float maxHealth = 100;
+    public float health;
+    public float damageLerpSpeed;
+    public float lerpThreshold;
+
+    public int score;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health = max(0f, health - amount);
+        health = min(100f, health);
+    }
+
+    public void RecoverHealth(float amount)
+    {
+        health = min(100f, health + amount);
+        health = max(0f, health);
+    }
+}
