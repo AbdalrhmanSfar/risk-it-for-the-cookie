72cf80e77eb06e3e815f9375df926b7bd98329af
Sima <simasandouk@gmail.com>
2025-08-30 17:38:39 +0300

    Revert "Merge branch 'Abdalrhman2'"
    This reverts commit 1e0e536ba00f976f2e49e62e857db2649299655b, reversing
    changes made to 3152df18876cb11ddc116de43bd1284fea4c6da1.

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting.Antlr3.Runtime;
+using Dan.Main;
 using UnityEngine;
 using UnityEngine.Rendering;
 using static Unity.Mathematics.math;
@@ -6,20 +6,21 @@ using static Unity.Mathematics.math;
 public class LogicScript : MonoBehaviour
 {
     public GameObject spawner;
+    private const string highScoreKey = "Highscore";
+    private const string nameKey = "Name";
     // edit the variables on this script/this object only
-    public float gainedMalwareDamage = 20;
-    public float gainedCookieHealth = 10;
-    public float gainedEnergyCharge = 10;
-    public float maxHealth = 100;
-    private float health; // made private because we want to edit it exclusively from the functions
-    public float maxEnergy = 100;
-    private float energy; // made private because we want to edit it exclusively from the functions
+    public string userName;
+    public float gainedMalwareDamage;
+    public float gainedCookieHealth;
+    public float gainedEnergyCharge;
+    public float maxHealth;
+    private float health; // made private because we want to edit it exclusively with the functions
+    public float maxEnergy;
+    private float energy; // made private because we want to edit it exclusively with the functions
     public float energyNeededforDash;
-    public float healthLerpSpeed;
-    public float healthLerpThreshold;
     public float energyLerpSpeed;
     public float energyLerpThreshold;
-    public int score = 0;
+    public int score = 0, highScore;
     public float fallingStuffSpeed;
     public bool gameStarted = false;
     private float startTimer = 0;
@@ -30,14 +31,12 @@ public class LogicScript : MonoBehaviour
     public bool alive = true;
     public bool isJumping = false;
     public bool isWalking = false;
-    public float storeSpeed; // to freeze stuff when the game is paused 
-    public GameObject pauseScreen;
-    private bool paused = false;
-    public GameObject settingsScreen; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        userName = PlayerPrefs.GetString(nameKey, "nullName");
         health = maxHealth;
         energy = maxEnergy;
     }
@@ -55,8 +54,8 @@ public class LogicScript : MonoBehaviour
             else
                 startTimer += Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.Escape)&& !paused) { pause();  }
     }
+
 
     public float GetHealth()
     {
@@ -70,7 +69,7 @@ public class LogicScript : MonoBehaviour
     public void TakeDamage(float amount)
     {
         health = max(0f, health - amount);
-        health = min(100f, health);
+        health = min(maxHealth, health);
         if (health == 0)
         {
             GameOver();
@@ -79,52 +78,31 @@ public class LogicScript : MonoBehaviour
 
     public void RecoverHealth(float amount)
     {
-        health = min(100f, health + amount);
+        health = min(maxHealth, health + amount);
         health = max(0f, health);
     }
 
     public void DrainEnergy(float amount)
     {
-        energy = min(100f, energy - amount);
+        energy = min(maxEnergy, energy - amount);
         energy = max(0f, energy);
     }
 
     public void ChargeEnergy(float amount)
     {
-        energy = min(100f, energy + amount);
+        energy = min(maxEnergy, energy + amount);
         energy = max(0f, energy);
     }
 
     public void GameOver()
     {
+        highScore = max(score, highScore);
+        PlayerPrefs.SetInt(highScoreKey, highScore);
         Debug.Log("GAME OVER!");
         fallingStuffSpeed = 0;
         spawner.SetActive(false);
         alive = false;
+        Leaderboards.Cookie.UploadNewEntry(userName, highScore);
     }
 
-    public void pause() {
-        Debug.Log("Pause Screen");
-        storeSpeed = fallingStuffSpeed; fallingStuffSpeed = 0;
-        spawner.SetActive(false);
-        alive = false; pauseScreen.SetActive(true);
-        paused = true;
-    }
-
-    public void resume() {
-        Debug.Log("Resume Game");
-        fallingStuffSpeed = storeSpeed;
-        spawner.SetActive(true);  alive = true;
-        pauseScreen.SetActive(false);
-        paused = false;
-    }
-
-    public void settings() {
-        pauseScreen.SetActive(false);
-        settingsScreen.SetActive(true); 
-    }
-    public void settingBackButton() {
-        settingsScreen.SetActive(false);
-        pauseScreen.SetActive(true); 
-    }
 }
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -23,7 +23,6 @@ public class UIScript : MonoBehaviour
     public GameObject scoreText;
     private int score;
 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
